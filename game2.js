8a9c046b55c5fb56e6e9a395cca371141f442dbb
WazirDihaz <166924512+WazirDihaz@users.noreply.github.com>
2024-09-24 00:20:26 -0700

    Add files via upload

---
+++ game2.js
@@ -0,0 +1,24 @@
+console.log("i am still here");
+
+
+const obj=
+{
+idd:4,
+name:"zahid",
+classs:"software",
+location:"mirpur",
+city:"gilgit",
+functionmy:()=>
+{
+    console.log(" id "+this.idd+" class :" +this.classs+"locarin"+this.location);
+}
+}
+const obj2={
+   
+
+}
+obj2.__proto__=obj;
+
+
+ 
+
